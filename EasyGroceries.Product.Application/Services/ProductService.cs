564dc2390b86d283bd501334f6700c768a89f9f5
bhushan <bhushann@quinnox.com>
2023-10-18 13:58:02 +0530

    Product API changes

--- EasyGroceries.Product.Application/Services/ProductService.cs
+++ EasyGroceries.Product.Application/Services/ProductService.cs
@@ -2,11 +2,6 @@
 using EasyGroceries.Product.Application.DTOs;
 using EasyGroceries.Product.Application.Features.ProductInfo.Requests.Queries;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyGroceries.Product.Application.Services
 {
